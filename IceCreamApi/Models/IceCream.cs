939da5a1e88f4307fd5eca6688d4613e803a7216
Vitalik Knysh <knish.vital@gmail.com>
2021-12-10 01:34:56 +0200

    Add project files.

---
+++ IceCreamApi/Models/IceCream.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IceCreamApi.Models
+{
+    public class IceCream
+    {
+        public Guid Id { get; set; }
+        public string Flavour { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public int WeightInGrams { get; set; }
+    }
+}
---
+++ IceCreamApi/Models/IceCreamContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IceCreamApi.Models
+{
+    public class IceCreamContext : DbContext
+    {
+        public DbSet<IceCream> IceCreams { get; set; }
+
+        public IceCreamContext(DbContextOptions<IceCreamContext> options) : base(options)
+        {
+        }
+    }
+}
