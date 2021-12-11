49e1147e215733571a03c655827cdf67fd026f2c
Vitalik Knysh <knish.vital@gmail.com>
2021-12-11 19:22:19 +0200

    Add GET, POST, PUT, PATCH, DELETE requests

---
+++ IceCreamApi/Controllers/IceCreamsController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using IceCreamApi.Models;
+
+namespace IceCreamApi.Controllers
+{
+    [ApiController]
+    [Route("api/icecream")]
+    public class IceCreamsController : ControllerBase
+    {
+        private readonly IceCreamContext _context;
+
+        public IceCreamsController(IceCreamContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/icecream
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<IceCream>>> GetIceCreamsAsync()
+        {
+            return await _context.IceCreams.ToListAsync();
+        }
+
+        // GET api/icecream/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IceCream>> GetIceCreamByIdAsync(Guid id)
+        {
+            IceCream iceCream = await _context.IceCreams.FirstOrDefaultAsync(i => i.Id == id);
+
+            if (iceCream is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(iceCream);
+        }
+
+        // POST api/icecream
+        [HttpPost]
+        public async Task<ActionResult<IceCream>> CreateIceCreamAsync(IceCream iceCream)
+        {
+            _context.Add(iceCream);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetIceCreamByIdAsync), new { Id = iceCream.Id }, iceCream);
+        }
+
+        // PUT api/icecream/{id}
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateIceCreamAsync(Guid id, IceCream newIceCream)
+        {
+            IceCream iceCream = await _context.IceCreams.FirstOrDefaultAsync(i => i.Id == id);
+
+            if (iceCream is null)
+            {
+                return NotFound();
+            }
+
+            iceCream.Flavour = newIceCream.Flavour;
+            iceCream.Color = newIceCream.Color;
+            iceCream.Price = newIceCream.Price;
+            iceCream.WeightInGrams = newIceCream.WeightInGrams;
+
+            _context.Update(iceCream);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // PATCH api/icecream/{id}
+        [HttpPatch("{id}")]
+        public async Task<ActionResult> PartialUpdateIceCreamAsync(Guid id, 
+            JsonPatchDocument<IceCream> patchDoc)
+        {
+            IceCream iceCream = await _context.IceCreams.FirstOrDefaultAsync(i => i.Id == id);
+
+            if (iceCream is null)
+            {
+                return NotFound();
+            }
+
+            patchDoc.ApplyTo(iceCream, ModelState);
+
+            if (!TryValidateModel(iceCream))
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE api/icecream/{id}
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteIceCreamAsync(Guid id)
+        {
+            IceCream iceCream = await _context.IceCreams.FirstOrDefaultAsync(i => i.Id == id);
+
+            if (iceCream is null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(iceCream);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
--- IceCreamApi/Models/IceCream.cs
+++ IceCreamApi/Models/IceCream.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace IceCreamApi.Models
 {
     public class IceCream
     {
         public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
         public string Flavour { get; set; }
+
         public string Color { get; set; }
+
+        [Required]
+        [Range(1, 30)]
         public decimal Price { get; set; }
+
         public int WeightInGrams { get; set; }
     }
 }
