ffc6728625f516369f8ac4894274d4d222238f45
Vitalik Knysh <knish.vital@gmail.com>
2021-12-12 21:14:44 +0200

    Separate abstract from realization via repository creation

---
+++ IceCreamApi/Repositories/IControllerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using IceCreamApi.Models;
+
+namespace IceCreamApi.Repositories
+{
+    public interface IControllerRepository
+    {
+        Task<IEnumerable<IceCream>> GetAllIceCreamsAsync();
+        Task<IceCream> GetIceCreamByIdAsync(Guid id);
+        Task SaveChangesAsync();
+        void AddIceCream(IceCream iceCream);
+        void UpdateIceCream(IceCream iceCream);
+        void RemoveIceCream(IceCream iceCream);
+    }
+}
