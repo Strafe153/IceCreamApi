7f720f4f705d99b757fc9914c2f0910aaf904c84
Vitalik Knysh <knish.vital@gmail.com>
2022-02-02 02:36:09 +0200

    Make unit-tests asynchronous, remove redundancy

--- IceCreamApi.Tests/IceCreamsControllerTests.cs
+++ IceCreamApi.Tests/IceCreamsControllerTests.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.JsonPatch;
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using IceCreamApi.Dtos;
 using IceCreamApi.Models;
@@ -16,12 +17,14 @@ namespace IceCreamApi.Tests
 {
     public class IceCreamsControllerTests
     {
+        private static readonly Mock<IControllerRepository> _repo = new Mock<IControllerRepository>();
+        private static readonly Mock<IMapper> _mapper = new Mock<IMapper>();
+
         [Fact]
-        public void GetIceCreamsAsync_ExistingItems_ReturnsIceCreamReadDtos()
+        public async Task GetIceCreamsAsync_ExistingItems_ReturnsIceCreamReadDtos()
         {
             // Arrange
-            var repo = new Mock<IControllerRepository>();
-            repo.Setup(r => r.GetAllIceCreamsAsync()).ReturnsAsync(
+            _repo.Setup(r => r.GetAllIceCreamsAsync()).ReturnsAsync(
                 new List<IceCream>()
                 { 
                     new IceCream { Id =  Guid.Empty },
@@ -29,8 +32,7 @@ namespace IceCreamApi.Tests
                     new IceCream { Id =  Guid.Empty }
                 });
 
-            var mapper = new Mock<IMapper>();
-            mapper.Setup(m => m.Map<IEnumerable<IceCreamReadDto>>(
+            _mapper.Setup(m => m.Map<IEnumerable<IceCreamReadDto>>(
                 It.IsAny<IEnumerable<IceCream>>())).Returns(
                     new List<IceCreamReadDto>()
                     {
@@ -39,10 +41,10 @@ namespace IceCreamApi.Tests
                         new IceCreamReadDto()
                     });
 
-            var controller = new IceCreamsController(repo.Object, mapper.Object);
+            var controller = new IceCreamsController(_repo.Object, _mapper.Object);
 
             // Act
-            var result = controller.GetIceCreamsAsync().Result;
+            var result = await controller.GetIceCreamsAsync();
 
             // Assert
             Assert.IsAssignableFrom<IEnumerable<IceCreamReadDto>>(result);
@@ -50,180 +52,155 @@ namespace IceCreamApi.Tests
         }
 
         [Fact]
-        public void GetIceCreamByIdAsync_ExistingItem_ReturnsActionResultOfIceCreamReadDto()
+        public async Task GetIceCreamByIdAsync_ExistingItem_ReturnsActionResultOfIceCreamReadDto()
         {
             // Arrange
-            var repo = new Mock<IControllerRepository>();
-            repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
+            _repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
                 .ReturnsAsync(new IceCream());
 
-            var mapper = new Mock<IMapper>();
-
-            var controller = new IceCreamsController(repo.Object, mapper.Object);
+            var controller = new IceCreamsController(_repo.Object, _mapper.Object);
 
             // Act
-            var result = controller.GetIceCreamByIdAsync(Guid.Empty).Result;
+            var result = await controller.GetIceCreamByIdAsync(Guid.Empty);
 
             // Assert
             Assert.IsType<ActionResult<IceCreamReadDto>>(result);
         }
 
         [Fact]
-        public void CreateIceCreamAsync_ValidDto_ReturnsActionRestulOfIceCream()
+        public async Task CreateIceCreamAsync_ValidDto_ReturnsActionRestulOfIceCream()
         {
             // Arrange
-            var repo = new Mock<IControllerRepository>();
-
-            var mapper = new Mock<IMapper>();
-            mapper.Setup(m => m.Map<IceCreamReadDto>(It.IsAny<IceCream>()))
+            _mapper.Setup(m => m.Map<IceCreamReadDto>(It.IsAny<IceCream>()))
                 .Returns(new IceCreamReadDto() with { Id = Guid.Empty });
 
-            var controller = new IceCreamsController(repo.Object, mapper.Object);
+            var controller = new IceCreamsController(_repo.Object, _mapper.Object);
 
             // Act
-            var result = controller.CreateIceCreamAsync(new IceCreamCreateUpdateDto()).Result;
+            var result = await controller.CreateIceCreamAsync(new IceCreamCreateUpdateDto());
 
             // Assert
             Assert.IsType<ActionResult<IceCream>>(result);
         }
 
         [Fact]
-        public void UpdateIceCreamAsync_ExistingItem_ReturnsNoContentResult()
+        public async Task UpdateIceCreamAsync_ExistingItem_ReturnsNoContentResult()
         {
             // Arrange
-            var repo = new Mock<IControllerRepository>();
-            repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
+            _repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
                 .ReturnsAsync(new IceCream());
 
-            var mapper = new Mock<IMapper>();
-
-            var controller = new IceCreamsController(repo.Object, mapper.Object);
+            var controller = new IceCreamsController(_repo.Object, _mapper.Object);
 
             // Act
-            var result = controller.UpdateIceCreamAsync(Guid.Empty, new IceCreamCreateUpdateDto()).Result;
+            var result = await controller.UpdateIceCreamAsync(Guid.Empty, new IceCreamCreateUpdateDto());
 
             // Assert
             Assert.IsType<NoContentResult>(result);
         }
 
         [Fact]
-        public void UpdateIceCreamAsync_NonexistingItem_ReturnsNotFoundResult()
+        public async Task UpdateIceCreamAsync_NonexistingItem_ReturnsNotFoundResult()
         {
             // Arrange
-            var repo = new Mock<IControllerRepository>();
-            repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
+            _repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
                 .ReturnsAsync((IceCream)null);
 
-            var mapper = new Mock<IMapper>();
-
-            var controller = new IceCreamsController(repo.Object, mapper.Object);
+            var controller = new IceCreamsController(_repo.Object, _mapper.Object);
 
             // Act
-            var result = controller.UpdateIceCreamAsync(Guid.Empty, new IceCreamCreateUpdateDto()).Result;
+            var result = await controller.UpdateIceCreamAsync(Guid.Empty, new IceCreamCreateUpdateDto());
 
             // Assert
             Assert.IsType<NotFoundResult>(result);
         }
 
         [Fact]
-        public void PartialUpdateIceCreamAsync_ExistingItem_ReturnsNoContentResult()
+        public async Task PartialUpdateIceCreamAsync_ExistingItem_ReturnsNoContentResult()
         {
             // Arrange
-            var repo = new Mock<IControllerRepository>();
-            repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
+            _repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
                 .ReturnsAsync(new IceCream());
 
-            var mapper = new Mock<IMapper>();
-            mapper.Setup(m => m.Map<IceCreamCreateUpdateDto>(It.IsAny<IceCream>()))
+            _mapper.Setup(m => m.Map<IceCreamCreateUpdateDto>(It.IsAny<IceCream>()))
                 .Returns(new IceCreamCreateUpdateDto());
 
-            var controller = new IceCreamsController(repo.Object, mapper.Object);
+            var controller = new IceCreamsController(_repo.Object, _mapper.Object);
             MockObjectModelValidator(controller);
 
             // Act
-            var result = controller.PartialUpdateIceCreamAsync(Guid.Empty, 
-                new JsonPatchDocument<IceCreamCreateUpdateDto>()).Result;
+            var result = await controller.PartialUpdateIceCreamAsync(Guid.Empty, 
+                new JsonPatchDocument<IceCreamCreateUpdateDto>());
 
             // Assert
             Assert.IsType<NoContentResult>(result);
         }
 
         [Fact]
-        public void PartialUpdateIceCreamAsync_NonexistingItem_ReturnsNotFoundResult()
+        public async Task PartialUpdateIceCreamAsync_NonexistingItem_ReturnsNotFoundResult()
         {
             // Arrange
-            var repo = new Mock<IControllerRepository>();
-            repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
+            _repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
                 .ReturnsAsync((IceCream)null);
 
-            var mapper = new Mock<IMapper>();
-
-            var controller = new IceCreamsController(repo.Object, mapper.Object);
+            var controller = new IceCreamsController(_repo.Object, _mapper.Object);
 
             // Act
-            var result = controller.PartialUpdateIceCreamAsync(Guid.Empty,
-                new JsonPatchDocument<IceCreamCreateUpdateDto>()).Result;
+            var result = await controller.PartialUpdateIceCreamAsync(Guid.Empty,
+                new JsonPatchDocument<IceCreamCreateUpdateDto>());
 
             // Assert
             Assert.IsType<NotFoundResult>(result);
         }
 
         [Fact]
-        public void PartialUpdateIceCreamAsync_ExistingItemInvalidModel_ReturnsActionResult()
+        public async Task PartialUpdateIceCreamAsync_ExistingItemInvalidModel_ReturnsActionResult()
         {
             // Arrange
-            var repo = new Mock<IControllerRepository>();
-            repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
+            _repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
                 .ReturnsAsync(new IceCream());
 
-            var mapper = new Mock<IMapper>();
-            mapper.Setup(m => m.Map<IceCreamCreateUpdateDto>(It.IsAny<IceCream>()))
+            _mapper.Setup(m => m.Map<IceCreamCreateUpdateDto>(It.IsAny<IceCream>()))
                 .Returns(new IceCreamCreateUpdateDto());
 
-            var controller = new IceCreamsController(repo.Object, mapper.Object);
+            var controller = new IceCreamsController(_repo.Object, _mapper.Object);
             MockObjectModelValidator(controller);
 
             // Act
-            var result = controller.PartialUpdateIceCreamAsync(Guid.Empty,
-                new JsonPatchDocument<IceCreamCreateUpdateDto>()).Result;
+            var result = await controller.PartialUpdateIceCreamAsync(Guid.Empty,
+                new JsonPatchDocument<IceCreamCreateUpdateDto>());
 
             // Assert
             Assert.IsAssignableFrom<ActionResult>(result);
         }
 
         [Fact]
-        public void DeleteIceCreamAsync_ExistingItem_ReturnsNoContentResult()
+        public async Task DeleteIceCreamAsync_ExistingItem_ReturnsNoContentResult()
         {
             // Arrange
-            var repo = new Mock<IControllerRepository>();
-            repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
+            _repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
                 .ReturnsAsync(new IceCream());
 
-            var mapper = new Mock<IMapper>();
-
-            var controller = new IceCreamsController(repo.Object, mapper.Object);
+            var controller = new IceCreamsController(_repo.Object, _mapper.Object);
 
             // Act
-            var result = controller.DeleteIceCreamAsync(Guid.Empty).Result;
+            var result = await controller.DeleteIceCreamAsync(Guid.Empty);
 
             // Assert
             Assert.IsAssignableFrom<NoContentResult>(result);
         }
 
         [Fact]
-        public void DeleteIceCreamAsync_NonexistingItem_ReturnsNotFoundResult()
+        public async Task DeleteIceCreamAsync_NonexistingItem_ReturnsNotFoundResult()
         {
             // Arrange
-            var repo = new Mock<IControllerRepository>();
-            repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
+            _repo.Setup(r => r.GetIceCreamByIdAsync(It.IsAny<Guid>()))
                 .ReturnsAsync((IceCream)null);
 
-            var mapper = new Mock<IMapper>();
-
-            var controller = new IceCreamsController(repo.Object, mapper.Object);
+            var controller = new IceCreamsController(_repo.Object, _mapper.Object);
 
             // Act
-            var result = controller.DeleteIceCreamAsync(Guid.Empty).Result;
+            var result = await controller.DeleteIceCreamAsync(Guid.Empty);
 
             // Assert
             Assert.IsAssignableFrom<NotFoundResult>(result);
